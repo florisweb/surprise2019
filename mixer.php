c4480d851ed91cd85822d28a86fce5cc084b2af5
florisweb <github@florisweb.tk>
2019-11-23 14:56:15 +0100

    Setupcode

---
+++ mixer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/mainUI.css">
+		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' name='viewport'/>
+		<meta name="theme-color" content="#636ad5">
+		<style>
+
+
+		</style>
+		<title>Surprise 2019</title>
+	</head>
+	<body>
+		<div id="topBar">
+		</div>
+
+		<div id="mainContentHolder">
+			<div class="centerAligner" id="mainContent"></div>
+			<div id="mainContent"></div>
+			
+		</div>
+
+
+		<div id="bottomBar">
+			<div class="centerAligner" style="max-width: 160px">
+				<div class="button bDefault clickable">Voeg ingredient toe</div>
+			</div>
+		</div>
+
+
+		<script>
+		</script>
+
+
+	</body>
+</html>
